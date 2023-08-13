3baf0b94abecd41b17015616d6d0e7bc27955871
nthnn <nathanneisip@gmail.com>
2023-08-13 22:24:44 +0800

    Webpack configuration file.

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'production',
+    entry: {
+        main: './src/main.ts',
+    },
+    output: {
+        path: path.resolve(__dirname, './build'),
+        clean: true,
+        filename: 'zync.js'
+    },
+    resolve: {
+        extensions: [".ts", ".tsx", ".js"],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: "ts-loader"
+            }
+        ]
+    }
+};
