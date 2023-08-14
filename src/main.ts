fc7c3fd2cdd79c6a64469ae277d698d09a8d257c
nthnn <nathanneisip@gmail.com>
2023-08-15 04:35:13 +0800

    Hello world generation test.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,19 @@
+import {
+    BasicBlock,
+    ConstantInt,
+    Function,
+    FunctionType,
+    IRBuilder,
+    Module,
+    verifyModule
+} from 'llvm-bindings';
+
 import { TokenUtil } from './token';
 import { Tokenizer, TokenizerResult } from './tokenizer';
+import { ExprASTString } from './ast_expr';
+
+import CStdlib from './cstdlib';
+import LLVMGlobalContext from './llvm_context';
 
 function main(): void {
     console.log('Yttria Programming Language Compiler v0.0.1');
@@ -23,6 +37,46 @@ function main(): void {
             console.log('[line ' + error.line + ', column ' + error.column + '] ' + error.message);
         });
     }
+
+    const module: Module = new Module('test', LLVMGlobalContext);
+    const builder: IRBuilder = new IRBuilder(LLVMGlobalContext);
+
+    const expr1: ExprASTString = new ExprASTString("Value is '%s'");
+    const expr2: ExprASTString = new ExprASTString("Hello, world!");
+
+    const main: BasicBlock = BasicBlock.Create(
+        LLVMGlobalContext,
+        'entry',
+        Function.Create(
+            FunctionType.get(
+                builder.getInt32Ty(),
+                [],
+                false
+            ),
+            Function.LinkageTypes.ExternalLinkage,
+            'main',
+            module
+        )
+    );
+
+    builder.SetInsertPoint(main);
+    builder.CreateCall(
+        CStdlib.render(module),
+        [
+            expr1.visit(builder, module, main),
+            expr2.visit(builder, module, main)
+        ],
+        'printfCall',
+    );
+    builder.CreateRet(ConstantInt.get(builder.getInt32Ty(), 0, true));
+
+    const verifyResult = verifyModule(module);
+    if(verifyResult) {
+        console.log('LLVM Verification:\n' + verifyResult);
+        return;
+    }
+
+    console.log('\nModule:\n\n' + module.print());
 }
 
 main();
