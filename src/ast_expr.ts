ca97f9a576b2e6ba04003486688f6068b84f4607
nthnn <nathanneisip@gmail.com>
2023-08-15 18:19:32 +0800

    AST Expressions for string, int, and float.

--- src/ast_expr.ts
+++ src/ast_expr.ts
@@ -1,14 +1,18 @@
 import {
     BasicBlock,
     Constant,
+    ConstantFP,
+    ConstantInt,
     IRBuilder,
-    Module
+    Module,
+    Type
 } from "llvm-bindings";
 
 import { ExpressionAST } from "./ast";
 import { createHash } from "crypto";
 
 import DataType from "./data_type";
+import LLVMGlobalContext from "./llvm_context";
 
 class ExprASTString implements ExpressionAST {
     private value: string;
@@ -26,7 +30,8 @@ class ExprASTString implements ExpressionAST {
             this.value,
             '__' + createHash('md5')
                 .update(this.value)
-                .digest('hex'),
+                .digest('hex')
+                .substring(0, 10),
             0,
             module
         );
@@ -37,4 +42,87 @@ class ExprASTString implements ExpressionAST {
     }
 }
 
-export { ExprASTString };
+class ExprASTInt implements ExpressionAST {
+    private value: number;
+    private bit: number;
+
+    private static typeMap: Map<number, [Type, DataType]> =
+        new Map<number, [Type, DataType]>([
+        [4, [Type.getIntNTy(LLVMGlobalContext, 4), DataType.I4]],
+        [8, [Type.getInt8Ty(LLVMGlobalContext), DataType.I8]],
+        [16, [Type.getInt16Ty(LLVMGlobalContext), DataType.I16]],
+        [32, [Type.getInt32Ty(LLVMGlobalContext), DataType.I32]],
+        [64, [Type.getInt64Ty(LLVMGlobalContext),DataType.I64]],
+        [128, [Type.getInt128Ty(LLVMGlobalContext), DataType.I128]]
+    ]);
+
+    public constructor(value: number, bit: number) {
+        this.value = value;
+        this.bit = bit;
+    }
+
+    private appropriateType(): Type {
+        if(ExprASTInt.typeMap.has(this.bit))
+            return ExprASTInt.typeMap.get(this.bit)![0];
+
+        return Type.getIntNTy(LLVMGlobalContext, 0);
+    }
+    
+    public visit(
+        builder: IRBuilder,
+        module: Module,
+        block: BasicBlock
+    ): Constant {
+        return ConstantInt.get(
+            this.appropriateType(),
+            this.value,
+            true
+        );
+    }
+
+    public type(): DataType {
+        return ExprASTInt.typeMap.get(this.bit)![1];
+    }
+}
+
+class ExprASTFloat implements ExpressionAST {
+    private value: number;
+    private bit: number;
+
+    private static typeMap: Map<number, [Type, DataType]> =
+        new Map<number, [Type, DataType]>([
+        [16, [Type.getBFloatTy(LLVMGlobalContext), DataType.F16]],
+        [32, [Type.getFloatTy(LLVMGlobalContext), DataType.F32]],
+        [64, [Type.getDoubleTy(LLVMGlobalContext), DataType.F64]],
+        [128, [Type.getPPC_FP128Ty(LLVMGlobalContext), DataType.F128]],
+    ]);
+
+    public constructor(value: number, bit: number) {
+        this.value = value;
+        this.bit = bit;
+    }
+
+    private appropriateType(): Type {
+        if(ExprASTFloat.typeMap.has(this.bit))
+            return ExprASTFloat.typeMap.get(this.bit)![0];
+
+        return Type.getIntNTy(LLVMGlobalContext, 0);
+    }
+
+    public visit(
+        builder: IRBuilder,
+        module: Module,
+        block: BasicBlock
+    ): Constant {
+        return ConstantFP.get(
+            this.appropriateType(),
+            this.value
+        );
+    }
+
+    public type(): DataType {
+        return ExprASTFloat.typeMap.get(this.bit)![1];
+    }
+}
+
+export { ExprASTString, ExprASTInt, ExprASTFloat };
--- src/data_type.ts
+++ src/data_type.ts
@@ -6,6 +6,18 @@ class DataType {
     }
 
     public static STRING: DataType = new DataType("string");
+
+    public static I4: DataType = new DataType("i4");
+    public static I8: DataType = new DataType("i8");
+    public static I16: DataType = new DataType("i16");
+    public static I32: DataType = new DataType("i32");
+    public static I64: DataType = new DataType("i64");
+    public static I128: DataType = new DataType("i128");
+
+    public static F16: DataType = new DataType("f16");
+    public static F32: DataType = new DataType("f32");
+    public static F64: DataType = new DataType("f64");
+    public static F128: DataType = new DataType("f128");
 }
 
 export default DataType;
